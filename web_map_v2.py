8747ffb250c3f8117e12dc9fe9a7700c14923d92
Binaya Basnet <binayabasnetb.b@gmail.com>
2025-12-21 11:41:19 +0545

    Auto-sync: 2025-12-21 11:41:19.55

--- web_map_v2.py
+++ web_map_v2.py
@@ -167,7 +167,7 @@ footer_html = '''
     border-radius: 5px;
     box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
     z-index: 999;">
-    Updated: Dec 14, 2025<br>
+    Updated: Dec 19, 2025<br>
     Made by: <a href="https://www.linkedin.com/in/binaya-basnet" target="_blank" style="color: #0073b1; text-decoration: none;">
         Er. Binaya Basnet
     </a>
